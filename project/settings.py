43104815b54e92fc05b138ea55eb023389d63cec
Sam Feldman <feldmans@anl.gov>
2023-05-24 19:41:36 -0500

    updatde

--- project/settings.py
+++ project/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'password',
         'HOST': '0.0.0.0',
         'PORT': '5432',
     }
