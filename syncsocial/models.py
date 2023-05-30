5e179c5187dc806558844e5d425d85daf0049dae
OlorinTG <97272661+OlorinTG@users.noreply.github.com>
2023-05-30 17:09:08 -0500

    asd

--- syncsocial/models.py
+++ syncsocial/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class FreeDate(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='free_dates')
     date = models.DateField()
 
     def __str__(self):
@@ -10,3 +10,4 @@ class FreeDate(models.Model):
 class Friend(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_friends')
     friend = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friend_friends')
+    
